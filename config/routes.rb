67dd026772f1db545e13c25af3a16f29d17d26fe
cent285 <chris@ubuntu.(none)>
2012-03-16 00:32:24 -0700

    finalRails

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Studemo::Application.routes.draw do
   get "students/index"
-  match "students/newStudent" => "students#newStudent"
   match "students/createStudent" => "students#createStudent"
   match "students/selectStudent" => "students#selectStudent"
   match "students/deleteStudent" => "students#deleteStudent"
